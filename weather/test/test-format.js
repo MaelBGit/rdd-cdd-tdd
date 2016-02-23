f71dbe2e162a47004fe6119cb485c3047da3d866
Maxime Cuaboz <maxime.cuaboz@devinci.fr>
2016-02-23 15:59:04 +0100

    update tests

--- weather/test/test-format.js
+++ weather/test/test-format.js
@@ -1,6 +1,9 @@
 var assert = require('assert'),
     format = require('./../lib/format');
 
-describe('format', function () {
-    it('is a constructor of the Format object')
+    describe('format', function () {
+        it('is a constructor of the Format object', function () {
+            var flag = typeof(format) === 'function';
+            assert(flag);
+        });
 });
